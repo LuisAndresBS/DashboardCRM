7f3379449309aa9fa23796723a14c879f70ac6fe
Luis <luisandresbarja@gmail.com>
2024-06-18 07:23:10 -0400

    redireccionamiento de pagina principal a dashboard

--- DashboardAdminCRM/urls.py
+++ DashboardAdminCRM/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('dashboard/kpi-dashboard')),
     path('dashboard/', include('Dashboard.urls')),
 ]
